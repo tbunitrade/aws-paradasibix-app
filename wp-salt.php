51d06504e18fcd526ef70c89e84092d551bac6e1
tbunitrade <tbunitrade@gmail.com>
2026-05-24 15:19:10 +0300

    init re[op

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'h6oUK2HugDfoNehQw85R5PJQXwxF9zDEe5SH5jc52UueWmvzxzFfcQU4G7jfKuw3');
+define('SECURE_AUTH_KEY',  'yF3rgXf5eUV9NszevbJH4hjbHCxKYaBg67CL0EdIg3J6bvpXoycDPnFN72pu1zd4');
+define('LOGGED_IN_KEY',    '2z367Hg1C0SccGYhuDHgDYqXEfxSwPsKxs4Ms8syxUgAL1CaHAstSRAUN54obcT9');
+define('NONCE_KEY',        'eg6qH2LEJhc1I507g4GbzMsCh2PMcSUa78eNoEW03iXuX94c3n6699qYRH7U05FA');
+define('AUTH_SALT',        'UCMGqXcsDNeUW79RRjafE507qRKBRVCm1YNq1YtNgRH7RKqsELnKriNtHCSXWhJm');
+define('SECURE_AUTH_SALT', 'hyQsQumBGSiXDrfdvyf8bVDFCjGDbuLXM9rhHFo8Pq2UxS7Msesm4p9SeGGDSJBx');
+define('LOGGED_IN_SALT',   '85zg6qjye2eREJAoEp58INfKtqKMMo462YfvCapgimF7Bjf39R2KeBDAULIfd7VX');
+define('NONCE_SALT',       'vNuFUFhRCiMhFtLzDIGIComsUsGIYUg9pSWyAaXASai4tMdMSYXAzrho8Rupt1a5');
